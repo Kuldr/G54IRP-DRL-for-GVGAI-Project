9f335be73476aeab66effc1cdd5504a15196a1aa
Benjamin Charlton <kuldrgames@gmail.com>
2018-11-05 17:04:04 +0000

    Moved some values of randomAgent into constants

--- Code/randomAgent.py
+++ Code/randomAgent.py
@@ -2,6 +2,9 @@ import gym
 import gym_gvgai
 import random
 
+RENDER_TO_SCREEN = False
+GAME_TICKS = 2000
+
 # Get all of the GVGAI Environments
 envs = [env.id for env in gym.envs.registry.all() if env.id.startswith('gvgai')]
 
@@ -9,12 +12,16 @@ envs = [env.id for env in gym.envs.registry.all() if env.id.startswith('gvgai')]
 env = gym.make(random.choice(envs)) # gym.make('gvgai-aliens-lvl0-v0') <- Code to load a specific environment
 env.reset()
 
+# Print the current enivronment
+print(env)
+
 # Set the score counter to 0
 score = 0
-# Run game for 2000 game ticks
-for i in range(2000):
-    # Render the game to the screen
-    env.render()
+# Run game for a number game ticks
+for i in range(GAME_TICKS):
+    # Render the game to the screen if option is selected
+    if RENDER_TO_SCREEN:
+        env.render()
     # Select a random action from the action space
     action_id = env.action_space.sample()
     # Perform the action choosen and get the info from the environment
@@ -25,8 +32,9 @@ for i in range(2000):
     # Print the results of the action performed
     print("Action " + str(action_id) + " played at game tick " + str(i+1) + ", reward=" + str(reward) + ", new score=" + str(score))
     if isOver:
-        print("Game over at game tick " + str(i+1) + " with player " + info['winner'])
+        print("Game over at game tick " + str(i+1) + " with player " + info['winner'] + ". Score = " + str(score) + " at Env " + str(env))
         break
 
 # Close the environment render after game is finished
-env.close()
+if RENDER_TO_SCREEN:
+    env.close()
