889666e75233420f2e53c45e45467fceb531309b
Benjamin Charlton <kuldrgames@gmail.com>
2018-11-09 18:21:21 +0000

    Randomly selects actions from list of possible actions checking validity

--- Code/randomAgent.py
+++ Code/randomAgent.py
@@ -4,6 +4,7 @@ import random
 
 RENDER_TO_SCREEN = False
 GAME_TICKS = 2000
+POSSIBLE_ACTIONS = ["ACTION_NIL", "ACTION_USE", "ACTION_LEFT", "ACTION_RIGHT", "ACTION_DOWN", "ACTION_UP"]
 
 # Get all of the GVGAI Environments w/o ghostbuster as it crashes
 envs = [env.id for env in gym.envs.registry.all() if env.id.startswith('gvgai') and not env.id.startswith('gvgai-ghostbuster') ]
@@ -13,8 +14,8 @@ envs = [env.id for env in gym.envs.registry.all() if env.id.startswith('gvgai') 
 env = gym.make(random.choice(envs))
 env.reset()
 
-# Print the current enivronment
-print(env)
+# Get the dictionary of actions in the env with their ids
+actions = dict(zip(env.unwrapped.actions, [i for i in range(env.unwrapped.action_space.n)]))
 
 # Set the score counter to 0
 score = 0
@@ -23,15 +24,21 @@ for i in range(GAME_TICKS):
     # Render the game to the screen if option is selected
     if RENDER_TO_SCREEN:
         env.render()
-    # Select a random action from the action space
-    action_id = env.action_space.sample()
+
+    # Select a random action from all possible actions and check its valid
+    action = None
+    while action == None:
+        x = random.choice(POSSIBLE_ACTIONS)
+        if x in actions:
+            action = actions[x]
+
     # Perform the action choosen and get the info from the environment
-    state, reward, isOver, info = env.step(action_id)
+    state, reward, isOver, info = env.step(action)
     # Update the cumilative score based upon the reward given
     score += reward
 
     # Print the results of the action performed
-    print("Action " + str(action_id) + " played at game tick " + str(i+1) + ", reward=" + str(reward) + ", new score=" + str(score))
+    print("Action " + str(action) + " played at game tick " + str(i+1) + ", reward=" + str(reward) + ", new score=" + str(score))
     if isOver:
         print("Game over at game tick " + str(i+1) + " with player " + info['winner'] + ". Score = " + str(score) + " at Env " + str(env))
         break
