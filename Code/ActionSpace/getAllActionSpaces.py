616d064c7ad8424a513776775d7c812ebdc0df4d
Benjamin Charlton <kuldrgames@gmail.com>
2018-11-10 13:57:31 +0000

    Added some basic print outs to keep track of progress

--- Code/ActionSpace/getAllActionSpaces.py
+++ Code/ActionSpace/getAllActionSpaces.py
@@ -1,8 +1,13 @@
 import gym
 import gym_gvgai
 import json
+import datetime
 import os
 import sys
+
+# Get and print the start time
+startTime = datetime.datetime.now()
+print("Start Time: %s" % startTime.time())
 
 # Get all of the GVGAI Environments w/o ghostbuster as it crashes
 envs = [env.id for env in gym.envs.registry.all() if env.id.startswith('gvgai') and not env.id.startswith('gvgai-ghostbuster') ]
@@ -11,12 +16,12 @@ envs = [env.id for env in gym.envs.registry.all() if env.id.startswith('gvgai') 
 actionsResults = []
 
 # Test every environment
-for x in envs:
+for i, env in enumerate(envs):
 
     # Make the environment
     original_stdout = sys.stdout
     sys.stdout = open(os.devnull, 'w')
-    env = gym.make(x)
+    env = gym.make(env)
     sys.stdout.close()
     sys.stdout = original_stdout
 
@@ -27,6 +32,13 @@ for x in envs:
                      "Actions" : env.unwrapped.actions}
     actionsResults.append(actionsResult)
 
+    print("%d/%d Environments Evalutated, Elapsed Time: %s"
+        % ((i+1), len(envs), datetime.datetime.now() - startTime))
+
 # Save as a json file
 with open("actionSpaceResults.json", "w") as outfile:
     json.dump(actionsResults, outfile, indent = "\t")
+
+# Print the running time
+endTime = datetime.datetime.now()
+print("Duration: %s" % (endTime - startTime))
