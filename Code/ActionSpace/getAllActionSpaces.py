69afa8c3fea8c1769f43a2d49ec6676f9c65fd8a
Benjamin Charlton <kuldrgames@gmail.com>
2018-11-10 13:14:04 +0000

    Suppressing output from the gym server opening

--- Code/ActionSpace/getAllActionSpaces.py
+++ Code/ActionSpace/getAllActionSpaces.py
@@ -1,6 +1,8 @@
 import gym
 import gym_gvgai
 import json
+import os
+import sys
 
 # Get all of the GVGAI Environments w/o ghostbuster as it crashes
 envs = [env.id for env in gym.envs.registry.all() if env.id.startswith('gvgai') and not env.id.startswith('gvgai-ghostbuster') ]
@@ -12,7 +14,11 @@ actionsResults = []
 for x in envs:
 
     # Make the environment
+    original_stdout = sys.stdout
+    sys.stdout = open(os.devnull, 'w')
     env = gym.make(x)
+    sys.stdout.close()
+    sys.stdout = original_stdout
 
     # Wrap the relevant info in a dictionary and append it to the results list
     actionsResult = {"Game" : env.unwrapped.game,
