b70fb92510ddc370c3ff5f7988ef5b00ca1d20b3
Benjamin Charlton <kuldrgames@gmail.com>
2019-01-24 17:29:30 +0000

    begun refactoring to match agent structure | first half of random agent

---
+++ Code/Agents/randomAgent.py
@@ -0,0 +1,31 @@
+from random import choice
+
+# TODO: Comments
+
+# Constants
+POSSIBLE_ACTIONS = ["ACTION_NIL", "ACTION_USE", "ACTION_LEFT",
+                    "ACTION_RIGHT", "ACTION_DOWN", "ACTION_UP"]
+
+# Gobal Variables
+actionsDict = None
+
+class Agent():
+
+    def __init__(self):
+        self.name = "randomAgentKuldr"
+        global actionsDict
+        actionsDict = None
+
+    def act(self, stateObs, actions):
+        global actionsDict
+        if actionsDict == None:
+            print("Setting up the action dictionary")
+            actionsDict = dict(zip(actions, [i for i in range(len(actions))]))
+
+        actionID = None
+        while actionID == None:
+            x = choice(POSSIBLE_ACTIONS)
+            if x in actionsDict:
+                actionID = actionsDict[x]
+
+        return actionID
