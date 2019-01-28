fcd7b309f9e1329ea7ab74a5bf596b66707ff331
Benjamin Charlton <kuldrgames@gmail.com>
2019-01-28 16:17:42 +0000

    Added comments for the randomAgent

--- Code/Agents/randomAgent.py
+++ Code/Agents/randomAgent.py
@@ -1,30 +1,34 @@
 from random import choice
 
-# TODO: Comments
-
 # Constants
-POSSIBLE_ACTIONS = ["ACTION_NIL", "ACTION_USE", "ACTION_LEFT",
+POTENTIAL_ACTIONS = ["ACTION_NIL", "ACTION_USE", "ACTION_LEFT",
                     "ACTION_RIGHT", "ACTION_DOWN", "ACTION_UP"]
-
 # Gobal Variables
 actionsDict = None
 
 class Agent():
-
+    # Initialisation of the agent
+    # should be called everytime the environment is reset
     def __init__(self):
         self.name = "randomAgentKuldr"
         global actionsDict
         actionsDict = None
 
+    # Ask the agent for an action
+    # passing in a state observation and a list of valid actions
     def act(self, stateObs, actions):
+        # check if the valid action dictionary has been created if not make it
         global actionsDict
         if actionsDict == None:
             actionsDict = dict(zip(actions, [i for i in range(len(actions))]))
 
+        # Select a random action from the list of all potential actions
+        # Keep trying until a valid action is selected
         actionID = None
         while actionID == None:
-            x = choice(POSSIBLE_ACTIONS)
+            x = choice(POTENTIAL_ACTIONS)
             if x in actionsDict:
                 actionID = actionsDict[x]
 
+        # Return the chosen action 
         return actionID
