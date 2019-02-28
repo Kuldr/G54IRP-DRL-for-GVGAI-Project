bb62dd63c78465ba25892b282886c81d83fb30c2
Benjamin Charlton <kuldrgames@gmail.com>
2019-02-28 16:38:29 +0000

    Added function to scale rewards based on max score

--- Code/Test/modelHelperFunctions.py
+++ Code/Test/modelHelperFunctions.py
@@ -8,3 +8,20 @@ def transformFrame(frame, x, y):
     frameIm = frameIm.resize((x,y))
     frame = np.asarray(frameIm)
     return frame
+
+# NOTE: THIS DOESN'T SCALE -IVE REWARDS BASED ON MIN SCORE BUT ON MAX SCORE
+def normalizeReward(reward, envString):
+    newReward = reward / MAX_SCORES[envString]
+    print("Reward: " + str(reward) + " | newReward: " + str(newReward) + " | scale: " + str(MAX_SCORES[envString]) + " | env: " + envString)
+    return newReward
+
+MAX_SCORES = {"<TimeLimit<GVGAI_Env<gvgai-boulderdash-lvl0-v0>>>" : 22,
+		      "<TimeLimit<GVGAI_Env<gvgai-boulderdash-lvl1-v0>>>" : 22,
+              "<TimeLimit<GVGAI_Env<gvgai-boulderdash-lvl2-v0>>>" : 22,
+              "<TimeLimit<GVGAI_Env<gvgai-boulderdash-lvl3-v0>>>" : 22,
+              "<TimeLimit<GVGAI_Env<gvgai-boulderdash-lvl4-v0>>>" : 22,
+              "<TimeLimit<GVGAI_Env<gvgai-missilecommand-lvl0-v0>>>" : 8,
+              "<TimeLimit<GVGAI_Env<gvgai-missilecommand-lvl1-v0>>>" : 16,
+              "<TimeLimit<GVGAI_Env<gvgai-missilecommand-lvl2-v0>>>" : 8,
+              "<TimeLimit<GVGAI_Env<gvgai-missilecommand-lvl3-v0>>>" : 8,
+              "<TimeLimit<GVGAI_Env<gvgai-missilecommand-lvl4-v0>>>" : 14}
